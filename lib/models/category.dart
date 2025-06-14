aa1bdb4985cfb16f1b008e94e9a7cc27d10d85e9
Salvo <salvatoresalanitro94@gmail.com>
2025-06-14 11:36:14 +0200

    add initial models and data

---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  const Category(
+    this.categoryName,
+    this.color
+  );
+
+  final String categoryName;
+  final Color color;
+}
