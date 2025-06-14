3b502f1c5349fef848068bb0fa19cb3b5e236135
Salvo <salvatoresalanitro94@gmail.com>
2025-06-14 12:29:47 +0200

    convert groceries list in stateful and add newItem in actions on appBar

--- lib/widgets/groceries_list.dart
+++ lib/widgets/groceries_list.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widgets/new_item.dart';
 
-class GroceriesList extends StatelessWidget {
+class GroceriesList extends StatefulWidget {
   const GroceriesList({super.key});
+
+  @override
+  State<GroceriesList> createState() => _GroceriesListState();
+}
+
+class _GroceriesListState extends State<GroceriesList> {
+  void _addItem() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (ctx) => NewItem()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Groceries')),
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(onPressed: _addItem, icon: Icon(Icons.add)),
+        ],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (ctx, index) {
